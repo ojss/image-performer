ef08e14c634070fe692795414f66bbfab0aaf8fd
Ojas Shirekar <ojas.shirekar@gmail.com>
2021-03-30 15:40:21 +0200

    add performer modifications and add trainer opts
    opts added: --img64 <path to img net root> --performer

--- train_transformer.py
+++ train_transformer.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import argparse
 import torch
 import sys
@@ -11,6 +13,7 @@ import tensorboardX
 import torch.optim as optim
 import torchvision
 from image_transformer import ImageTransformer
+from image_performer import ImagePerformer
 import matplotlib
 import itertools
 from torch.utils.data import DataLoader
@@ -19,11 +22,12 @@ from torchviz import make_dot
 from tqdm import tqdm
 import torch.nn as nn
 
+from pathlib import Path
+
 matplotlib.use('Agg')
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 sns.set()
+
 
 def dict2namespace(config):
     namespace = argparse.Namespace()
@@ -35,18 +39,29 @@ def dict2namespace(config):
         setattr(namespace, key, new_value)
     return namespace
 
+
 def parse_args_and_config():
     """
     :return args, config: namespace objects that stores information in args and config files.
     """
     parser = argparse.ArgumentParser(description=globals()['__doc__'])
 
-    parser.add_argument('--config', type=str, default='transformer_dmol.yml', help='Path to the config file')
-    parser.add_argument('--doc', type=str, default='0', help='A string for documentation purpose')
-    parser.add_argument('--verbose', type=str, default='info', help='Verbose level: info | debug | warning | critical')
-    parser.add_argument('--sample', action='store_true', help='Sample at train time')
+    parser.add_argument(
+        '--config', type=str, default='transformer_dmol.yml', help='Path to the config file')
+    parser.add_argument('--doc', type=str, default='0',
+                        help='A string for documentation purpose')
+    parser.add_argument('--verbose', type=str, default='info',
+                        help='Verbose level: info | debug | warning | critical')
+    parser.add_argument('--sample', action='store_true',
+                        help='Sample at train time')
+    parser.add_argument('--performer', action="store_true",
+                        help="Chooses the performer based model")
+    parser.add_argument("--img64", type=Path, help="Path to img net", default=None)
 
     args = parser.parse_args()
+
+    print(args.img64)
+
     args.log = os.path.join('transformer_logs', args.doc)
     # parse config file
     with open(os.path.join('configs', args.config), 'r') as f:
@@ -68,7 +83,8 @@ def parse_args_and_config():
 
     handler1 = logging.StreamHandler()
     handler2 = logging.FileHandler(os.path.join(args.log, 'stdout.txt'))
-    formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
     handler1.setFormatter(formatter)
     handler2.setFormatter(formatter)
     logger = logging.getLogger()
@@ -77,7 +93,8 @@ def parse_args_and_config():
     logger.setLevel(level)
 
     # add device information to args
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device(
+        'cuda') if torch.cuda.is_available() else torch.device('cpu')
     logging.info("Using device: {}".format(device))
     new_config.device = device
 
@@ -89,16 +106,19 @@ def parse_args_and_config():
 
     return args, new_config
 
+
 def get_lr(step, config):
     warmup_steps = config.optim.warmup
-    lr_base = config.optim.lr * 0.002 # for Adam correction
+    lr_base = config.optim.lr * 0.002  # for Adam correction
     ret = 5000. * config.model.hidden_size ** (-0.5) * \
-          np.min([(step + 1) * warmup_steps ** (-1.5), (step + 1) ** (-0.5)])
+        np.min([(step + 1) * warmup_steps ** (-1.5), (step + 1) ** (-0.5)])
     return ret * lr_base
+
 
 def main():
     args, config = parse_args_and_config()
-    tb_logger = tensorboardX.SummaryWriter(log_dir=os.path.join('transformer_logs', args.doc))
+    tb_logger = tensorboardX.SummaryWriter(
+        log_dir=os.path.join('transformer_logs', args.doc))
 
     if config.model.distr == "dmol":
         # Scale size and rescale data to [-1, 1]
@@ -113,12 +133,39 @@ def main():
             transforms.Resize(config.model.image_size),
             transforms.ToTensor()
         ])
-    dataset = datasets.CIFAR10('datasets/transformer', transform=transform, download=True)
-    loader = DataLoader(dataset, batch_size=config.train.batch_size, shuffle=True, num_workers=4)
-    input_dim = config.model.image_size ** 2 * config.model.channels
-    model = ImageTransformer(config.model).to(config.device)
-    optimizer = optim.Adam(model.parameters(), lr=1., betas=(0.9, 0.98), eps=1e-9)
-    scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda step: get_lr(step, config))
+    
+    if args.img64 is None:
+        dataset = datasets.CIFAR10(
+            'datasets/transformer', transform=transform, download=True)
+        loader = DataLoader(
+            dataset, batch_size=config.train.batch_size, shuffle=True, num_workers=4)
+        input_dim = config.model.image_size ** 2 * config.model.channels
+        model = ImagePerformer(config.model).to(
+            config.device) if args.performer else ImageTransformer(config.model).to(config.device)
+        optimizer = optim.Adam(model.parameters(), lr=1.,
+                            betas=(0.9, 0.98), eps=1e-9)
+        scheduler = optim.lr_scheduler.LambdaLR(
+            optimizer, lr_lambda=lambda step: get_lr(step, config))
+    else:
+        train_dir = args.img64/'train'
+        val_dir = args.img64/'val'
+        transform = transforms.Compose([
+            transforms.Resize(config.model.image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+
+        dataset = datasets.ImageFolder(train_dir, transform=transform)
+        loader = DataLoader(dataset, batch_size=config.train.batch_size, shuffle=True, num_workers=4)
+        input_dim = config.model.image_size ** 2 * config.model.channels
+        model = ImagePerformer(config.model).to(
+            config.device) if args.performer else ImageTransformer(config.model).to(config.device)
+        optimizer = optim.Adam(model.parameters(), lr=1.,
+                            betas=(0.9, 0.98), eps=1e-9)
+        scheduler = optim.lr_scheduler.LambdaLR(
+            optimizer, lr_lambda=lambda step: get_lr(step, config))
+
 
     # Initialize as in their code
     gain = config.model.initializer_gain
@@ -129,9 +176,10 @@ def main():
         # if "attn" in name and "output" not in name:
         #     nn.init.xavier_normal_(p)
         if p.dim() > 1:
-            nn.init.xavier_uniform_(p, gain=np.sqrt(gain)) # Need sqrt for inconsistency between pytorch / TF
+            # Need sqrt for inconsistency between pytorch / TF
+            nn.init.xavier_uniform_(p, gain=np.sqrt(gain))
         else:
-            a =  np.sqrt(3. * gain / p.shape[0])
+            a = np.sqrt(3. * gain / p.shape[0])
             nn.init.uniform_(p, -a, a)
 
     # Accumulate data statistics for debugging purposes, e.g. to analyze the entropy of the first dimension
@@ -148,21 +196,22 @@ def main():
             return input * 0.5 + 0.5
 
     step = 0
-    losses_per_dim = torch.zeros(config.model.channels, config.model.image_size, config.model.image_size).to(config.device)
+    losses_per_dim = torch.zeros(
+        config.model.channels, config.model.image_size, config.model.image_size).to(config.device)
     for _ in range(config.train.epochs):
         for _, (imgs, l) in enumerate(loader):
             imgs = imgs.to(config.device)
             model.train()
-
-            scheduler.step()
             optimizer.zero_grad()
             preds = model(imgs)
             loss = model.loss(preds, imgs)
             decay = 0. if step == 0 else 0.99
             if config.model.distr == "dmol":
-                losses_per_dim[0,:,:] = losses_per_dim[0,:,:] * decay + (1 - decay) * loss.detach().mean(0) / np.log(2)
+                losses_per_dim[0, :, :] = losses_per_dim[0, :, :] * \
+                    decay + (1 - decay) * loss.detach().mean(0) / np.log(2)
             else:
-                losses_per_dim = losses_per_dim * decay + (1 - decay) * loss.detach().mean(0) / np.log(2)
+                losses_per_dim = losses_per_dim * decay + \
+                    (1 - decay) * loss.detach().mean(0) / np.log(2)
             loss = loss.view(loss.shape[0], -1).sum(1)
             loss = loss.mean(0)
 
@@ -179,7 +228,8 @@ def main():
             total_norm = (total_norm ** (1. / 2))
 
             if config.train.clip_grad_norm > 0.0:
-                nn.utils.clip_grad_norm_(model.parameters(), config.train.clip_grad_norm)
+                nn.utils.clip_grad_norm_(
+                    model.parameters(), config.train.clip_grad_norm)
 
             total_norm_post = 0
             for p in model.parameters():
@@ -188,6 +238,7 @@ def main():
             total_norm_post = (total_norm_post ** (1. / 2))
 
             optimizer.step()
+            scheduler.step()
             bits_per_dim = loss / (np.log(2.) * input_dim)
             acc = model.accuracy(preds, imgs)
 
@@ -195,21 +246,26 @@ def main():
                 logging.info('step: {}; loss: {:.3f}; bits_per_dim: {:.3f}, acc: {:.3f}, grad norm pre: {:.3f}, post: {:.3f}'
                              .format(step, loss.item(), bits_per_dim.item(), acc.item(), total_norm, total_norm_post))
                 tb_logger.add_scalar('loss', loss.item(), global_step=step)
-                tb_logger.add_scalar('bits_per_dim', bits_per_dim.item(), global_step=step)
+                tb_logger.add_scalar(
+                    'bits_per_dim', bits_per_dim.item(), global_step=step)
                 tb_logger.add_scalar('acc', acc.item(), global_step=step)
                 tb_logger.add_scalar('grad_norm', total_norm, global_step=step)
 
             if step % config.train.sample_iter == 0:
                 logging.info("Sampling from model: {}".format(args.doc))
                 if config.model.distr == "cat":
-                    channels = ['r','g','b']
+                    channels = ['r', 'g', 'b']
                     color_codes = ['Reds', "Greens", 'Blues']
                     for idx, c in enumerate(channels):
-                        ax = sns.heatmap(losses_per_dim[idx,:,:].cpu().numpy(), linewidth=0.5, cmap=color_codes[idx])
-                        tb_logger.add_figure("losses_per_dim/{}".format(c), ax.get_figure(), close=True, global_step=step)
+                        ax = sns.heatmap(losses_per_dim[idx, :, :].cpu(
+                        ).numpy(), linewidth=0.5, cmap=color_codes[idx])
+                        tb_logger.add_figure(
+                            "losses_per_dim/{}".format(c), ax.get_figure(), close=True, global_step=step)
                 else:
-                    ax = sns.heatmap(losses_per_dim[0,:,:].cpu().numpy(), linewidth=0.5, cmap='Blues')
-                    tb_logger.add_figure("losses_per_dim", ax.get_figure(), close=True, global_step=step)
+                    ax = sns.heatmap(losses_per_dim[0, :, :].cpu(
+                    ).numpy(), linewidth=0.5, cmap='Blues')
+                    tb_logger.add_figure(
+                        "losses_per_dim", ax.get_figure(), close=True, global_step=step)
 
                 model.eval()
                 with torch.no_grad():
@@ -218,27 +274,38 @@ def main():
                     tb_logger.add_image('imgs', imgs_grid, global_step=step)
 
                     # Evaluate model predictions for the input
-                    pred_samples = revert_samples(model.sample_from_preds(preds))
-                    pred_samples_grid = torchvision.utils.make_grid(pred_samples[:8, ...], 3)
-                    tb_logger.add_image('pred_samples/random', pred_samples_grid, global_step=step)
-                    pred_samples = revert_samples(model.sample_from_preds(preds, argmax=True))
-                    pred_samples_grid = torchvision.utils.make_grid(pred_samples[:8, ...], 3)
-                    tb_logger.add_image('pred_samples/argmax', pred_samples_grid, global_step=step)
+                    pred_samples = revert_samples(
+                        model.sample_from_preds(preds))
+                    pred_samples_grid = torchvision.utils.make_grid(
+                        pred_samples[:8, ...], 3)
+                    tb_logger.add_image(
+                        'pred_samples/random', pred_samples_grid, global_step=step)
+                    pred_samples = revert_samples(
+                        model.sample_from_preds(preds, argmax=True))
+                    pred_samples_grid = torchvision.utils.make_grid(
+                        pred_samples[:8, ...], 3)
+                    tb_logger.add_image(
+                        'pred_samples/argmax', pred_samples_grid, global_step=step)
 
                     if args.sample:
-                        samples = revert_samples(model.sample(config.train.sample_size, config.device))
-                        samples_grid = torchvision.utils.make_grid(samples[:8, ...], 3)
-                        tb_logger.add_image('samples', samples_grid, global_step=step)
+                        samples = revert_samples(model.sample(
+                            config.train.sample_size, config.device))
+                        samples_grid = torchvision.utils.make_grid(
+                            samples[:8, ...], 3)
+                        tb_logger.add_image(
+                            'samples', samples_grid, global_step=step)
 
                     # Argmax samples are not useful for unconditional generation
                     # if config.model.distr == "cat":
                     #     argmax_samples = model.sample(1, config.device, argmax=True)
                     #     samples_grid = torchvision.utils.make_grid(argmax_samples[:8, ...], 3)
                     #     tb_logger.add_image('argmax_samples', samples_grid, global_step=step)
-                torch.save(model.state_dict(), os.path.join('transformer_logs', args.doc, "model.pth"))
+                torch.save(model.state_dict(), os.path.join(
+                    'transformer_logs', args.doc, "model.pth"))
             step += 1
 
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
